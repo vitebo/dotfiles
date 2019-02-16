d8e7794b84d7a6e822561a1e35c6abcafc10502a
vitebo <vitebo51@gmail.com>
2019-02-16 13:11:02 -0200

    install node 10.15.1

--- install.sh
+++ install.sh
@@ -18,3 +18,7 @@ ln -s ~/.dotfiles/.profile ~/.profile
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
+# install node
+nvm install 10.15.1
+nvm alias default 10.15.1
+
